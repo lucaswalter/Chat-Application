8b0895071a41d16d3fb67ecdb9a068dda49855b8
Andrew Kwan <akck9@mst.edu>
2017-04-25 15:56:05 -0500

    Room has id and ways to interact with members.

--- Server/Room.cs
+++ Server/Room.cs
@@ -9,22 +9,26 @@ public class Room
 {
     private Queue<Message> recent;  // Queue of most recent messages
     private string name;            // Name of the room
-    private bool isPrivate;         // Boolean for room access type
+    private int id;                 // Room ID
     private string owner;           // Owner's username/id
-    private List<string> users;     // List of users (names/ids)
     // private List<string> admins;     // List of admins (names/ids)
     private DateTime openTime;      // Time that room was opened
-
+    
+    public bool isPrivate;         // Boolean for room access type
+    public List<string> users;     // List of users (names/ids)
+    
     // Constructs default room
     public Room () {
         owner = "Server";
         name = "Room";
+        id = 0;
         openTime = DateTime.Now;
     }
-    // Constructs a room with owner 'o', name 'n', and created at time 't'
-    public Room (string o, string n, DateTime t){
+    // Constructs a room with owner 'o', name 'n', and created at time 't', and id 'i'
+    public Room (string o, string n, DateTime t, int i){
         owner = o;
         name = n;
+        id = i;
         openTime = t;
     }
 
@@ -35,6 +39,13 @@ public class Room
     // Returns room's name.
     public string getName(){ 
         return name;
+    }
+    
+    public string getOwner(){
+        return owner;
+    }
+    public int getID(){
+        return id;
     }
 
     // Returns TimeSpan since last message
