c0db61a2a14ac0c4621e1efff21ea06d4bc31db0
Lucas Wyland <lucaswyland11@gmail.com>
2017-04-26 02:15:18 -0500

    Implemented User Login Window

--- Client/LoginWindow.xaml.cs
+++ Client/LoginWindow.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Client
 {
@@ -22,6 +10,15 @@ namespace Client
         public LoginWindow()
         {
             InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string userName = loginTextBox.Text.Trim();
+
+            this.Hide();
+            MainWindow mainWindow = new MainWindow(userName);
+            mainWindow.Show();         
         }
     }
 }
